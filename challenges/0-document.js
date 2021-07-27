401925e5ca2336f00dcc3e89e1b1290f13289343
ibrahimabdu <ibrahim.abdu777@gmail.com>
2021-07-27 15:02:51 +0100

    added some JSDoc comments

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,13 @@
 // TODO (Ex.0): document and write tests for this function
-
+/**
+ * 
+ * @param {number} n - number that will become negative
+ * @returns {number} - negative versions of input n
+ */
 function asNegative(n) {
-  // ...
+  if (n > 0) return -n
+  else return n
 }
+
 
 
